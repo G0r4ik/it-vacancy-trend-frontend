bfdc0f85833ca5817aaf2397f67032f336267a7c
Egor Gora <egorgorlushkin0@gmail.com>
2022-10-05 18:12:31 +0300

    add favicon test opengraph

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
+import './style/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 const app = createApp(App)
 createApp(App).mount('#app')
-
-import 'flatpickr/dist/flatpickr.css'
-import './style/main.css'
