fac67e80c0e0a39af8744c356928036f057972e2
Egor Gor <egorgorlushkin0@gmail.com>
2024-02-02 14:40:25 +0300

    fix: Исправление багов - в шапке теперь появляется крестик - добавлены в игнор некоторые ошибки из doiuse - исправлены повторные запросы когда данные уже загружены.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   plugins: env === 'production' && [
     require('doiuse')({
       ignoreFiles: ['**/normalize.css', '**/node_modules/**/*.css'],
+      ignore: ['css-gradients', 'css-sticky'],
     }),
     require('@fullhuman/postcss-purgecss')({
       content: ['./src/**/*.*', './node_modules/flatpickr/dist/flatpickr.css'],
